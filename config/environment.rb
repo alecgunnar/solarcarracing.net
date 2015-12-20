3edde9c05ffc5241f69ec02bc83552296fa58190
gunnar94 <gunnar94@me.com>
2015-12-20 16:33:22 -0500

    Now loading private config

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
+require 'yaml'
+
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+
+if File.exists?('config/private.yml')
+    PRIVATE = YAML.load('config/private.yml')
+end
 
 # Initialize the Rails application.
 Rails.application.initialize!
