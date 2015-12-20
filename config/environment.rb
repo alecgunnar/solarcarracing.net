67e13dc23200ba6cb253dfc49ddffa5d222deb6b
gunnar94 <gunnar94@me.com>
2015-12-20 16:45:41 -0500

    Updated database and configuration
    - Now using MySQL
    - Added db connection info to private config dist file
    - Added code to load private config

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,14 @@ require 'yaml'
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-if File.exists?('config/private.yml')
-    PRIVATE = YAML.load('config/private.yml')
+PRIVATE_CONFIG_FILE = "#{Rails.root.to_s}/config/private.yml";
+
+unless File.exists?(PRIVATE_CONFIG_FILE)
+    puts 'The private configuration file does not exist... Run `bin/setup` to create it!'
+    exit
 end
+
+PRIVATE = YAML.load_file(PRIVATE_CONFIG_FILE)
 
 # Initialize the Rails application.
 Rails.application.initialize!
