97327394dd2af4858258bb641ab2aa462ef0cd1a
gunnar94 <gunnar94@me.com>
2015-12-20 22:16:12 -0500

    Confirmable works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
