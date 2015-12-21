ba02ddad303dfd02072587e791237313777efa23
gunnar94 <gunnar94@me.com>
2015-12-21 10:41:23 -0500

    Made the forms look nice

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'welcome#homepage'
 
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords'
   }
 end
