0e12436fd2b8db32033738d27f9804049b33879e
gunnar94 <gunnar94@me.com>
2015-12-27 14:24:26 -0500

    Disabled devise user edit page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_sign_up_params, only: [:create]
 
+  def edit
+    not_found
+  end
+
   protected
     def configure_sign_up_params
       devise_parameter_sanitizer.for(:sign_up) << :username
