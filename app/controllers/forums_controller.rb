d6b3621792416da228d8949dcbec799d85d71af1
gunnar94 <gunnar94@me.com>
2015-12-29 13:20:49 -0500

    Changed some colors

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -5,6 +5,8 @@ class ForumsController < ApplicationController
 
   def show
     @forum  = Forum.find params[:id]
+
+    # Doesn't use the last_post.post_date because newly created topics to not have a last_post!
     @topics = Topic.joins(:replies).where('topics.forum_id = ?', @forum.id).order('posts.post_date DESC').uniq
   end
 end
